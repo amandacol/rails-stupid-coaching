7bf13cf1a6237942accda02d9a1ab30b61d94556
Amanda Bezerra <colmenero88@bol.com.br>
2020-02-17 14:56:54 -0300

    done

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class QuestionsControllerTest < ActionDispatch::IntegrationTest
   test "should get ask" do
-    get questions_ask_url
+    get ask_url
     assert_response :success
   end
 
   test "should get answer" do
-    get questions_answer_url
+    get answer_url(question: 'hello')
     assert_response :success
   end
 
